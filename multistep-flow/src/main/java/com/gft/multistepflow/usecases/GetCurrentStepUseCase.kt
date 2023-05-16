312cfc42b4b90f8615300d249b2d070fe05dd40f
Krzysztof Lopacki <krzysztof.lopacki@gft.com>
2023-05-16 15:06:54 +0200

    Improve casting in GetCurrentStepUseCase

--- multistep-flow/src/main/java/com/gft/multistepflow/usecases/GetCurrentStepUseCase.kt
+++ multistep-flow/src/main/java/com/gft/multistepflow/usecases/GetCurrentStepUseCase.kt
@@ -1,9 +1,11 @@
 package com.gft.multistepflow.usecases
 
 import com.gft.multistepflow.model.MultiStepFlow
+import com.gft.multistepflow.model.Step
 import com.gft.multistepflow.model.StepType
 
 open class GetCurrentStepUseCase<FlowStepType : StepType<*, *, *, *>>(private val flow: MultiStepFlow<FlowStepType>) {
-    operator fun invoke() = flow.session.data.value?.currentStep
+    @Suppress("UNCHECKED_CAST")
+    operator fun invoke(): Step<FlowStepType, *, *, *, *> = (flow.session.data.value?.currentStep as Step<FlowStepType, *, *, *, *>?)
         ?: throw IllegalStateException("The flow has not started yet or has ended already")
 }
