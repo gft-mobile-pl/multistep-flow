32c0f3430f88af184acd8603465258eab2a38ef1
Krzysztof Lopacki <krzysztof.lopacki@gft.com>
2023-08-31 17:31:47 +0200

    Make IsFlowStartedUseCase open

--- multistep-flow/src/main/java/com/gft/multistepflow/usecases/IsFlowStartedUseCase.kt
+++ multistep-flow/src/main/java/com/gft/multistepflow/usecases/IsFlowStartedUseCase.kt
@@ -3,7 +3,7 @@ package com.gft.multistepflow.usecases
 import com.gft.multistepflow.model.MultiStepFlow
 import kotlinx.coroutines.flow.map
 
-class IsFlowStartedUseCase(
+open class IsFlowStartedUseCase(
     private val flow: MultiStepFlow<*>
 ) {
     operator fun invoke() = flow.session.data.map { state -> state != null }
