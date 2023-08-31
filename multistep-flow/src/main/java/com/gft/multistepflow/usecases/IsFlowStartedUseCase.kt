40985c05e4fd188144c07fdfaf90cb20e6df6090
Krzysztof Lopacki <krzysztof.lopacki@gft.com>
2023-08-31 12:13:19 +0200

    Add IsFlowStartedUseCase use case

---
+++ multistep-flow/src/main/java/com/gft/multistepflow/usecases/IsFlowStartedUseCase.kt
@@ -0,0 +1,10 @@
+package com.gft.multistepflow.usecases
+
+import com.gft.multistepflow.model.MultiStepFlow
+import kotlinx.coroutines.flow.map
+
+class IsFlowStartedUseCase(
+    private val flow: MultiStepFlow<*>
+) {
+    operator fun invoke() = flow.session.data.map { state -> state != null }
+}
