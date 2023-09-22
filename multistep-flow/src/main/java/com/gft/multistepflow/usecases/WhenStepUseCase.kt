b97be259834610b3eb532ad52cf11f541a1cd56e
Krzysztof Lopacki <krzysztof.lopacki@gft.com>
2023-09-22 14:03:14 +0200

    Add skipIfAnyOperationInProgress parameter to WhenStepUseCase

--- multistep-flow/src/main/java/com/gft/multistepflow/usecases/WhenStepUseCase.kt
+++ multistep-flow/src/main/java/com/gft/multistepflow/usecases/WhenStepUseCase.kt
@@ -10,56 +10,87 @@ open class WhenStepUseCase(private val flow: MultiStepFlow<*>) {
     operator fun <T : StepType<out Payload, out UserInput, out ValidationResult, *>, Payload, UserInput, ValidationResult> invoke(
         stepType: T,
         vararg stepTypes: T,
-        action: (Step<T, out Payload, out UserInput, out ValidationResult, *>) -> Unit
-    ) = flow.session.data.value?.currentStep?.castOrNull(stepType, *stepTypes)?.let(action) ?: Unit
+        skipIfAnyOperationInProgress: Boolean = true,
+        block: (Step<T, out Payload, out UserInput, out ValidationResult, *>) -> Unit
+    ) = flow.session.data.value?.run {
+        if (skipIfAnyOperationInProgress && isAnyOperationInProgress) return
+        currentStep.castOrNull(stepType, *stepTypes)?.let(block)
+    } ?: Unit
 
     @JvmName("invoke_Same_Payload")
     operator fun <T : StepType<Payload, out UserInput, out ValidationResult, *>, Payload, UserInput, ValidationResult> invoke(
         stepType: T,
         vararg stepTypes: T,
-        action: (Step<T, out Payload, out UserInput, out ValidationResult, *>) -> Unit
-    ) = flow.session.data.value?.currentStep?.castOrNull(stepType, *stepTypes)?.let(action) ?: Unit
-
+        skipIfAnyOperationInProgress: Boolean = true,
+        block: (Step<T, out Payload, out UserInput, out ValidationResult, *>) -> Unit
+    ) = flow.session.data.value?.run {
+        if (skipIfAnyOperationInProgress && isAnyOperationInProgress) return
+        currentStep.castOrNull(stepType, *stepTypes)?.let(block)
+    } ?: Unit
 
     @JvmName("invoke_Same_Payload_UserInput")
     operator fun <T : StepType<Payload, UserInput, out ValidationResult, *>, Payload, UserInput, ValidationResult> invoke(
         stepType: T,
         vararg stepTypes: T,
-        action: (Step<T, out Payload, out UserInput, out ValidationResult, *>) -> Unit
-    ) = flow.session.data.value?.currentStep?.castOrNull(stepType, *stepTypes)?.let(action) ?: Unit
+        skipIfAnyOperationInProgress: Boolean = true,
+        block: (Step<T, out Payload, out UserInput, out ValidationResult, *>) -> Unit
+    ) = flow.session.data.value?.run {
+        if (skipIfAnyOperationInProgress && isAnyOperationInProgress) return
+        currentStep.castOrNull(stepType, *stepTypes)?.let(block)
+    } ?: Unit
 
     @JvmName("invoke_Same_Payload_ValidationResult")
     operator fun <T : StepType<Payload, out UserInput, ValidationResult, *>, Payload, UserInput, ValidationResult> invoke(
         stepType: T,
         vararg stepTypes: T,
-        action: (Step<T, out Payload, out UserInput, out ValidationResult, *>) -> Unit
-    ) = flow.session.data.value?.currentStep?.castOrNull(stepType, *stepTypes)?.let(action) ?: Unit
+        skipIfAnyOperationInProgress: Boolean = true,
+        block: (Step<T, out Payload, out UserInput, out ValidationResult, *>) -> Unit
+    ) = flow.session.data.value?.run {
+        if (skipIfAnyOperationInProgress && isAnyOperationInProgress) return
+        currentStep.castOrNull(stepType, *stepTypes)?.let(block)
+    } ?: Unit
 
     @JvmName("invoke_Same_Payload_UserInput_ValidationResult")
     operator fun <T : StepType<Payload, UserInput, ValidationResult, *>, Payload, UserInput, ValidationResult> invoke(
         stepType: T,
         vararg stepTypes: T,
-        action: (Step<T, out Payload, out UserInput, out ValidationResult, *>) -> Unit
-    ) = flow.session.data.value?.currentStep?.castOrNull(stepType, *stepTypes)?.let(action) ?: Unit
+        skipIfAnyOperationInProgress: Boolean = true,
+        block: (Step<T, out Payload, out UserInput, out ValidationResult, *>) -> Unit
+    ) = flow.session.data.value?.run {
+        if (skipIfAnyOperationInProgress && isAnyOperationInProgress) return
+        currentStep.castOrNull(stepType, *stepTypes)?.let(block)
+    } ?: Unit
 
     @JvmName("invoke_Same_UserInput")
     operator fun <T : StepType<out Payload, UserInput, out ValidationResult, *>, Payload, UserInput, ValidationResult> invoke(
         stepType: T,
         vararg stepTypes: T,
-        action: (Step<T, out Payload, out UserInput, out ValidationResult, *>) -> Unit
-    ) = flow.session.data.value?.currentStep?.castOrNull(stepType, *stepTypes)?.let(action) ?: Unit
+        skipIfAnyOperationInProgress: Boolean = true,
+        block: (Step<T, out Payload, out UserInput, out ValidationResult, *>) -> Unit
+    ) = flow.session.data.value?.run {
+        if (skipIfAnyOperationInProgress && isAnyOperationInProgress) return
+        currentStep.castOrNull(stepType, *stepTypes)?.let(block)
+    } ?: Unit
 
     @JvmName("invoke_Same_UserInput_ValidationResult")
     operator fun <T : StepType<out Payload, UserInput, ValidationResult, *>, Payload, UserInput, ValidationResult> invoke(
         stepType: T,
         vararg stepTypes: T,
-        action: (Step<T, out Payload, out UserInput, out ValidationResult, *>) -> Unit
-    ) = flow.session.data.value?.currentStep?.castOrNull(stepType, *stepTypes)?.let(action) ?: Unit
+        skipIfAnyOperationInProgress: Boolean = true,
+        block: (Step<T, out Payload, out UserInput, out ValidationResult, *>) -> Unit
+    ) = flow.session.data.value?.run {
+        if (skipIfAnyOperationInProgress && isAnyOperationInProgress) return
+        currentStep.castOrNull(stepType, *stepTypes)?.let(block)
+    } ?: Unit
 
     @JvmName("invoke_Same_ValidationResult")
     operator fun <T : StepType<out Payload, out UserInput, ValidationResult, *>, Payload, UserInput, ValidationResult> invoke(
         stepType: T,
         vararg stepTypes: T,
-        action: (Step<T, out Payload, out UserInput, out ValidationResult, *>) -> Unit
-    ) = flow.session.data.value?.currentStep?.castOrNull(stepType, *stepTypes)?.let(action) ?: Unit
+        skipIfAnyOperationInProgress: Boolean = true,
+        block: (Step<T, out Payload, out UserInput, out ValidationResult, *>) -> Unit
+    ) = flow.session.data.value?.run {
+        if (skipIfAnyOperationInProgress && isAnyOperationInProgress) return
+        currentStep.castOrNull(stepType, *stepTypes)?.let(block)
+    } ?: Unit
 }
