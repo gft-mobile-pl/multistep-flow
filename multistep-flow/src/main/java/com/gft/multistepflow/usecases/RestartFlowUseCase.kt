5c9a532d4a32f408567df4f43de88edbb3842a4b
Mykyta Sikriier <sikrinick@gmail.com>
2024-09-11 14:43:43 +0200

    Change `RestartFlowUseCase` to `open`

--- multistep-flow/src/main/java/com/gft/multistepflow/usecases/RestartFlowUseCase.kt
+++ multistep-flow/src/main/java/com/gft/multistepflow/usecases/RestartFlowUseCase.kt
@@ -5,7 +5,7 @@ import com.gft.multistepflow.model.Step
 import com.gft.multistepflow.model.StepType
 import kotlinx.coroutines.sync.withLock
 
-class RestartFlowUseCase<FlowStepType : StepType<*, *, *, *>>(
+open class RestartFlowUseCase<FlowStepType : StepType<*, *, *, *>>(
     private val flow: MultiStepFlow<FlowStepType>,
 ) {
     suspend operator fun <StepType : FlowStepType> invoke(
