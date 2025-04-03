c557df6f87b700453de4bf25608cde88092be64c
Krzysztof Lopacki <krzysztof.lopacki@gft.com>
2025-04-03 11:50:25 +0200

    Rethrow CancellationException while creating ActionError

--- multistep-flow/src/main/kotlin/com/gft/multistepflow/ActionError.kt
+++ multistep-flow/src/main/kotlin/com/gft/multistepflow/ActionError.kt
@@ -1,8 +1,14 @@
 package com.gft.multistepflow
 
+import kotlinx.coroutines.CancellationException
+
 class ActionError(
     val error: Throwable,
     val action: Action<*, *>,
     val retryAllowed: Boolean,
     val transactionId: String
-) : RuntimeException(error)
+) : RuntimeException(error) {
+    init {
+        if (error is CancellationException) throw error
+    }
+}
