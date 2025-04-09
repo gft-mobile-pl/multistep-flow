e0624e5c54fc88b99bfc526a9299cbea52a79ad8
Krzysztof Lopacki <krzysztof.lopacki@gft.com>
2025-04-09 12:49:31 +0200

    Allow starting flow by providing steps history instead of initial step

--- multistep-flow/src/main/kotlin/com/gft/multistepflow/operations/StartFlow.kt
+++ multistep-flow/src/main/kotlin/com/gft/multistepflow/operations/StartFlow.kt
@@ -37,4 +37,37 @@ class StartFlow<FlowStepType : StepType<*, *, *, *>> internal constructor(
             Result.failure(IllegalFlowStateException("Flow $flow is already started!"))
         }
     }
+
+    suspend operator fun invoke(
+        stepsHistory: List<Step<FlowStepType, *, *, *, *>>
+    ): Result<Unit> {
+        if (!flow.historyEnabled)
+            throw IllegalFlowException("Cannot start the flow using provided steps history as steps history feature is disabled for the current flow.")
+        if (stepsHistory.isEmpty())
+            throw IllegalArgumentException("Cannot start the flow as the provided steps history is empty. Initial step is required  to start a flow.")
+        stepsHistory.forEach { step ->
+            if (step.flow != null && step.flow != flow) {
+                throw IllegalArgumentException("Step can be added to one flow only. Copy the step if you need to add the same step to more than one flow.")
+            }
+        }
+
+        flow.lifecycle.first { state -> state !is Lifecycle.State.Clearing }
+
+        return try {
+            flow.session.start(
+                FlowState(
+                    currentStep = stepsHistory.last() as Step<*, *, *, *, *>,
+                    currentActionJob = null,
+                    currentActionType = null,
+                    stepsHistory = stepsHistory.onEach { step -> step.flow = flow },
+                    lifecycleState = Lifecycle.State.Started(UUID.randomUUID().toString()),
+                    error = null
+                )
+            )
+            Result.success(Unit)
+        } catch (error: Throwable) {
+            stepsHistory.onEach { step -> step.flow = null }
+            Result.failure(IllegalFlowStateException("Flow $flow is already started!"))
+        }
+    }
 }
